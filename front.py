958931da955bb58500e916b857369b3b2bc7d8d4
Jonathan Guerra <jonathanhttp123@gmail.com>
2023-02-13 01:22:54 -0500

    carga de imagenes

--- front.py
+++ front.py
@@ -20,66 +20,6 @@ def home():
    
     return render_template('home.html')
 
-#Documento
-@app.route('/success', methods = ["GET","POST"])  
-def success():  
-    if request.method == 'POST':  
-        f = request.files['file']
-        f.save(os.path.join(app.config['UPLOAD_FOLDER'],f.filename))
-        doc = os.path.join(app.config['UPLOAD_FOLDER'],f.filename)
-        racismo = backend.cargaListas(os.path.join(app.config['UPLOAD_FOLDER'],'racismo.txt'))
-        idenGenero = backend.cargaListas(os.path.join(app.config['UPLOAD_FOLDER'],'idenGen.txt'))
-        clase = backend.cargaListas(os.path.join(app.config['UPLOAD_FOLDER'],'clase.txt'))
-        edad = backend.cargaListas(os.path.join(app.config['UPLOAD_FOLDER'],'edad.txt'))
-        d1 = backend.cargaDoc(doc)
-        n = 1
-        """Construcción de Colección con listas discriminatorias"""
-        colecGeneral = backend.colecCompleta(d1,racismo,idenGenero,clase,edad)
-        """JACCARD"""
-        Jaccard = backend.jaccardCompleto(colecGeneral,1,2,0)
-        jacRes = backend.respuesta(Jaccard,n)
-        """Sorensen"""
-        Sorensen = backend.sorensenCompleto(colecGeneral,1,2,0)
-        sorRes = backend.respuesta(Sorensen,n)
-        """COSENO"""
-        Coseno = backend.cosenoVectN(colecGeneral,1,2,0)
-        cosRes = backend.respuesta(Coseno,n)
-        items,flag = backend.cargaDoc(doc)
-        print(items)
-        jacRes1 = (jacRes[0,:])
-        sorRes1 = (sorRes[0,:])
-        cosRes1 = (cosRes[0,:])
-        return render_template("docu.html",doc = items,jaccard = jacRes1, coseno = cosRes1, sorensen = sorRes1)
-
-#Colección
-@app.route('/success2', methods = ["GET","POST"])  
-def success2():  
-    if request.method == 'POST':  
-        f = request.files['file']
-        f.save(os.path.join(app.config['UPLOAD_FOLDER'],f.filename))
-        doc = os.path.join(app.config['UPLOAD_FOLDER'],f.filename)
-        racismo = backend.cargaListas(os.path.join(app.config['UPLOAD_FOLDER'],'racismo.txt'))
-        idenGenero = backend.cargaListas(os.path.join(app.config['UPLOAD_FOLDER'],'idenGen.txt'))
-        clase = backend.cargaListas(os.path.join(app.config['UPLOAD_FOLDER'],'clase.txt'))
-        edad = backend.cargaListas(os.path.join(app.config['UPLOAD_FOLDER'],'edad.txt'))
-        d1 = backend.cargaColec(doc)
-        n = len(d1[0])
-        """Construcción de Colección con listas discriminatorias"""
-        colecGeneral = backend.colecCompleta(d1,racismo,idenGenero,clase,edad)
-        """JACCARD"""
-        Jaccard = backend.jaccardCompleto(colecGeneral,1,2,0)
-        jacRes = np.round(backend.respuesta(Jaccard,n),2)
-        """Sorensen"""
-        Sorensen = backend.sorensenCompleto(colecGeneral,1,2,0)
-        sorRes = np.round(backend.respuesta(Sorensen,n),2)
-        """COSENO"""
-        Coseno = backend.cosenoVectN(colecGeneral,1,2,0)
-        cosRes = np.round(backend.respuesta(Coseno,n),2)
-        items,flag = backend.cargaColec(doc)
-        [x.encode('utf-8').decode('utf-8') for x in items]
-        print(items)
-        return render_template("coleccion.html",len=n,doc = items,jaccard = jacRes, coseno = cosRes, sorensen = sorRes)
-
 #Imagen
 @app.route('/success3', methods = ["GET","POST"])  
 def success3():  
@@ -87,30 +27,8 @@ def success3():
         f = request.files['file']
         f.save(os.path.join(app.config['UPLOAD_FOLDER'],f.filename))
         img = os.path.join(app.config['UPLOAD_FOLDER'],f.filename)
-        racismo = backend.cargaListas(os.path.join(app.config['UPLOAD_FOLDER'],'racismo.txt'))
-        idenGenero = backend.cargaListas(os.path.join(app.config['UPLOAD_FOLDER'],'idenGen.txt'))
-        clase = backend.cargaListas(os.path.join(app.config['UPLOAD_FOLDER'],'clase.txt'))
-        edad = backend.cargaListas(os.path.join(app.config['UPLOAD_FOLDER'],'edad.txt'))
-        d1 = backend.cargaDoc(img)
-        n = 1
-        """Construcción de Colección con listas discriminatorias"""
-        colecGeneral = backend.colecCompleta(d1,racismo,idenGenero,clase,edad)
-        """JACCARD"""
-        Jaccard = backend.jaccardCompleto(colecGeneral,1,2,0)
-        jacRes = backend.respuesta(Jaccard,n)
-        """Sorensen"""
-        Sorensen = backend.sorensenCompleto(colecGeneral,1,2,0)
-        sorRes = backend.respuesta(Sorensen,n)
-        """COSENO"""
-        Coseno = backend.cosenoVectN(colecGeneral,1,2,0)
-        cosRes = backend.respuesta(Coseno,n)
-        items,flag = backend.cargaDoc(img)
-        print(items)
-        jacRes1 = (jacRes[0,:])
-        sorRes1 = (sorRes[0,:])
-        cosRes1 = (cosRes[0,:])
-        return render_template("img.html",len=n,doc = items,jaccard = jacRes1, coseno = cosRes1, sorensen = sorRes1, img = img)
-
+        res = backend.run_example(app.config['UPLOAD_FOLDER'] +'\\'+ f.filename)
+        return render_template("home.html", imagen = img, respuesta = res)
 
 @app.route('/info')
 def info():
@@ -126,27 +44,8 @@ def info():
 #Frase
 @app.route('/submit', methods=["GET","POST"])
 def submit():
-    racismo = backend.cargaListas(os.path.join(app.config['UPLOAD_FOLDER'],'racismo.txt'))
-    idenGenero = backend.cargaListas(os.path.join(app.config['UPLOAD_FOLDER'],'idenGen.txt'))
-    clase = backend.cargaListas(os.path.join(app.config['UPLOAD_FOLDER'],'clase.txt'))
-    edad = backend.cargaListas(os.path.join(app.config['UPLOAD_FOLDER'],'edad.txt'))
-    d1 = (request.form['text'],1)
-    n = 1
-    #Construcción de Colección con listas discriminatorias
-    colecGeneral = backend.colecCompleta(d1,racismo,idenGenero,clase,edad)
-    #JACCARD
-    Jaccard = backend.jaccardCompleto(colecGeneral,1,2,0)
-    jacRes = backend.respuesta(Jaccard,n)
-    #Sorensen
-    Sorensen = backend.sorensenCompleto(colecGeneral,1,2,0)
-    sorRes = backend.respuesta(Sorensen,n)
-    #COSENO
-    Coseno = backend.cosenoVectN(colecGeneral,1,2,0)
-    cosRes = backend.respuesta(Coseno,n)
-    jacRes1 = (jacRes[0,:])
-    sorRes1 = (sorRes[0,:])
-    cosRes1 = (cosRes[0,:])
-    return render_template('resultados.html',result=request.form['text'],jaccard = jacRes1, coseno = cosRes1, sorensen = sorRes1)
+    
+    return render_template('resultados.html')
 
 
 ##ejecutar el servicio web
